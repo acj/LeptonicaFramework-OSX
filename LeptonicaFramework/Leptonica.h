fd10d042cd80382edf2544374ae3e8e4ba45eaba
Adam Jensen <acjensen@gmail.com>
2016-08-26 16:54:08 -0400

    Initial commit with functional macOS framework for Leptonica
    Leptonica source adapted from https://github.com/mcku/leptonica-osx .

---
+++ LeptonicaFramework/Leptonica.h
@@ -0,0 +1,19 @@
+//
+//  Leptonica.h
+//  Leptonica
+//
+//  Created by Adam Jensen on 8/26/16.
+//  Copyright © 2016 Adam Jensen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LeptonicaFramework.
+FOUNDATION_EXPORT double LeptonicaVersionNumber;
+
+//! Project version string for LeptonicaFramework.
+FOUNDATION_EXPORT const unsigned char LeptonicaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Leptonica/PublicHeader.h>
+
+#import <Leptonica/allheaders.h>
